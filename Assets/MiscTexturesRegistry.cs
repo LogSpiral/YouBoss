07a10bf8505be3a9c030c37f7eeae3c016ef4d98
Dominic <farfetchedking541@gmail.com>
2024-02-25 01:09:15 -0600

    Add generic textures and sounds

---
+++ Assets/MiscTexturesRegistry.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace YouBoss.Assets
+{
+    public class MiscTexturesRegistry : ModSystem
+    {
+        #region Texture Path Constants
+
+        public const string InvisiblePixelPath = $"{ExtraTexturesPath}/InvisiblePixel";
+
+        public const string ExtraTexturesPath = "NoxusBoss/Assets/ExtraTextures";
+
+        public const string GreyscaleTexturesPath = "NoxusBoss/Assets/ExtraTextures/GreyscaleTextures";
+
+        public const string NoiseTexturesPath = "NoxusBoss/Assets/ExtraTextures/Noise";
+
+        public const string ChromaticBurstPath = $"{GreyscaleTexturesPath}/ChromaticBurst";
+
+        #endregion Texture Path Constants
+
+        #region Greyscale Textures
+
+        public static readonly Texture2D BloomCircleSmall = LoadDeferred($"{GreyscaleTexturesPath}/BloomCircleSmall");
+
+        // General purpose greyscale bloom flare. Very useful for semi-weak overlays.
+        public static readonly Texture2D BloomFlare = LoadDeferred($"{GreyscaleTexturesPath}/BloomFlare");
+
+        public static readonly Texture2D ChromaticBurst = LoadDeferred(ChromaticBurstPath);
+
+        public static readonly Texture2D ShineFlareTexture = LoadDeferred($"{GreyscaleTexturesPath}/ShineFlare");
+
+        #endregion Greyscale Textures
+
+        #region Noise Textures
+
+        public static readonly Texture2D DendriticNoise = LoadDeferred($"{NoiseTexturesPath}/DendriticNoise");
+
+        public static readonly Texture2D TurbulentNoise = LoadDeferred($"{NoiseTexturesPath}/TurbulentNoise");
+
+        public static readonly Texture2D WavyBlotchNoise = LoadDeferred($"{NoiseTexturesPath}/WavyBlotchNoise");
+
+        #endregion Noise Textures
+
+        #region Invisible Pixel
+
+        // Self-explanatory. Sometimes shaders need a "blank slate" in the form of an invisible texture to draw their true contents onto, which this can be beneficial for.
+        public static readonly Texture2D InvisiblePixel = LoadDeferred(InvisiblePixelPath);
+
+        #endregion Invisible Pixel
+
+        #region Loader Utility
+
+        private static Texture2D LoadDeferred(string path)
+        {
+            // Don't attempt to load anything server-side.
+            if (Main.netMode == NetmodeID.Server)
+                return default;
+
+            return ModContent.Request<Texture2D>(path, AssetRequestMode.ImmediateLoad).Value;
+        }
+
+        #endregion Loader Utility
+    }
+}
---
+++ Assets/SoundsRegistry.cs
@@ -0,0 +1,49 @@
+﻿using Terraria.Audio;
+
+namespace YouBoss.Assets
+{
+    /// <summary>
+    /// Acts as a centralized storage for sounds where manual storing causes significant overhead.
+    /// </summary>
+    public static class SoundsRegistry
+    {
+        /// <summary>
+        /// Holds common, generic-use sound assets across the mod.
+        /// </summary>
+        public static class Common
+        {
+            public const string DirectoryBase = "YouBoss/Assets/Sounds/Custom/Common";
+
+            /// <summary>
+            /// A generic twinkle sound. Often used alongside a twinkle particle in some way.
+            /// </summary>
+            public static readonly SoundStyle TwinkleSound = new SoundStyle($"{DirectoryBase}/Twinkle") with { MaxInstances = 5, PitchVariance = 0.16f };
+        }
+
+        /// <summary>
+        /// Holds sounds specialized for use by terra blade boss.
+        /// </summary>
+        public static class TerraBlade
+        {
+            /// <summary>
+            /// The sound the blade makes when reforming from its three components.
+            /// </summary>
+            public static readonly SoundStyle BladeReformExplosionSound = new("YouBoss/Assets/Sounds/Custom/TerraBlade/BladeReformExplosion");
+
+            /// <summary>
+            /// The sound the blade makes when dashing.
+            /// </summary>
+            public static readonly SoundStyle DashSound = new("YouBoss/Assets/Sounds/Custom/TerraBlade/Dash");
+
+            /// <summary>
+            /// The sound the blade makes when splitting into its three components.
+            /// </summary>
+            public static readonly SoundStyle SplitSound = new("YouBoss/Assets/Sounds/Custom/TerraBlade/Split");
+
+            /// <summary>
+            /// The sound the blade makes when slashing.
+            /// </summary>
+            public static readonly SoundStyle SlashSound = new("YouBoss/Assets/Sounds/Custom/TerraBlade/Slash");
+        }
+    }
+}
