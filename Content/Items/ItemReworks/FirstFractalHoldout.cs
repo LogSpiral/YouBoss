535a0a781cacf410caefb646fb6410f961904b4d
Dominic <farfetchedking541@gmail.com>
2024-02-27 17:44:01 -0600

    Preparations for 3D rendering

--- Content/Items/ItemReworks/FirstFractalHoldout.cs
+++ Content/Items/ItemReworks/FirstFractalHoldout.cs
@@ -1,30 +1,67 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using YouBoss.Common.Tools.Easings;
+using YouBoss.Core.Graphics.Shaders;
 using YouBoss.Core.Graphics.Shaders.Screen;
 using static Humanizer.In;
+using static YouBoss.Core.Graphics.Primitives.PrimitiveTrail;
 
 namespace YouBoss.Content.Items.SummonItems
 {
     public class FirstFractalHoldout : ModProjectile
     {
-        public static int UseTime => SecondsToFrames(0.44f);
+        /// <summary>
+        /// The rotation of the sword in 3D space.
+        /// </summary>
+        public Quaternion Rotation
+        {
+            get;
+            set;
+        }
 
-        public static int BaseDamage => 256;
+        /// <summary>
+        /// How many swings have been performed thus far.
+        /// </summary>
+        public int SwingCounter
+        {
+            get;
+            set;
+        }
 
+        /// <summary>
+        /// The animation completion of the current sword swing.
+        /// </summary>
         public float AnimationCompletion => Saturate(Time / UseTime);
 
+        /// <summary>
+        /// The owner of this sword.
+        /// </summary>
         public Player Owner => Main.player[Projectile.owner];
 
+        /// <summary>
+        /// The animation timer of this sword, in frame.
+        /// </summary>
         public ref float Time => ref Projectile.ai[0];
 
-        public ref float ForwardDirection => ref Projectile.ai[1];
+        /// <summary>
+        /// The horizontal -1/1 direction that this sword is facing.
+        /// </summary>
+        public ref float HorizontalDirection => ref Projectile.ai[1];
 
+        /// <summary>
+        /// The starting Z rotation of this sword at the time of the animation starting.
+        /// </summary>
         public ref float StartingRotation => ref Projectile.ai[2];
+
+        public static int UseTime => SecondsToFrames(0.58f);
+
+        public static int BaseDamage => 256;
 
         public override string Texture => $"Terraria/Images/Item_{ItemID.FirstFractal}";
 
@@ -49,6 +86,7 @@ namespace YouBoss.Content.Items.SummonItems
             item.useAnimation = UseTime;
             item.useTurn = true;
             item.DamageType = DamageClass.MeleeNoSpeed;
+            item.UseSound = null;
             item.knockBack = 8f;
             item.autoReuse = true;
             item.noUseGraphic = true;
@@ -80,57 +118,138 @@ namespace YouBoss.Content.Items.SummonItems
             Projectile.noEnchantmentVisuals = true;
         }
 
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(SwingCounter);
+            writer.Write(Rotation.X);
+            writer.Write(Rotation.Y);
+            writer.Write(Rotation.Z);
+            writer.Write(Rotation.W);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            SwingCounter = reader.ReadInt32();
+
+            float x = reader.ReadSingle();
+            float y = reader.ReadSingle();
+            float z = reader.ReadSingle();
+            float w = reader.ReadSingle();
+            Rotation = new(x, y, z, w);
+        }
+
         public override void AI()
         {
             // Initialize directions.
-            if (ForwardDirection == 0f)
+            if (HorizontalDirection == 0f)
             {
                 StartingRotation = Projectile.velocity.ToRotation();
-                ForwardDirection = Projectile.velocity.X.NonZeroSign();
+                HorizontalDirection = Projectile.velocity.X.NonZeroSign();
+                Time = 0f;
+                Projectile.netUpdate = true;
             }
 
             DoBehavior_SwingForward();
 
             // Decide the arm rotation for the owner.
-            float armRotation = Projectile.rotation - (ForwardDirection == 1f ? PiOver2 : Pi);
+            float rotationZAngle = Atan2((Rotation.W * Rotation.Z + Rotation.X * Rotation.Y) * 2f, 1f - (Rotation.Y.Squared() + Rotation.Z.Squared()) * 2f);
+            float armRotation = rotationZAngle - (HorizontalDirection == 1f ? PiOver2 : Pi) - HorizontalDirection * PiOver4;
             Owner.SetCompositeArmFront(Math.Abs(armRotation) > 0.01f, Player.CompositeArmStretchAmount.Full, armRotation);
 
             // Glue the sword to its owner.
             Projectile.Center = Owner.RotatedRelativePoint(Owner.MountedCenter);
             Owner.heldProj = Projectile.whoAmI;
             Owner.SetDummyItemTime(2);
-            Owner.ChangeDir((int)ForwardDirection);
+            Owner.ChangeDir((int)HorizontalDirection);
 
             // Increment time and disappear once the AI timer has reached its maximum.
             Time++;
-            if (AnimationCompletion >= 1f)
-                Projectile.Kill();
+            if (Main.myPlayer == Projectile.owner && AnimationCompletion >= 1f)
+            {
+                // Check if the player is still using the sword. If they are, simply reset the AI timer.
+                // Otherwise, die.
+                if (Main.mouseLeft)
+                {
+                    Projectile.velocity = Projectile.DirectionToSafe(Main.MouseWorld);
+                    StartingRotation = Projectile.velocity.ToRotation();
+                    HorizontalDirection = Projectile.velocity.X.NonZeroSign();
+                    SwingCounter++;
+                    Time = 0f;
+                    Projectile.netUpdate = true;
+                }
+                else
+                    Projectile.Kill();
+            }
         }
 
         public void DoBehavior_SwingForward()
         {
-            float swingRotationOffset = Lerp(-3.2f, 1.6f, AnimationCompletion.Cubed());
-            Projectile.rotation = StartingRotation + swingRotationOffset * ForwardDirection;
+            if (Main.gameMenu)
+                return;
+
+            switch (SwingCounter % 1)
+            {
+                case 0:
+                    Quaternion start = EulerAnglesConversion(-0.06f, 0.12f);
+                    Quaternion anticipation = EulerAnglesConversion(-1.26f, 0.47f);
+                    Quaternion slash = EulerAnglesConversion(3.65f, 0.8f);
+                    Quaternion end = EulerAnglesConversion(3.79f, 0.15f);
+
+                    PiecewiseRotation rotation = new PiecewiseRotation().
+                        Add(PolynomialEasing.Quadratic, EasingType.Out, anticipation, 0.51f, start).
+                        Add(PolynomialEasing.Quartic, EasingType.In, slash, 0.9f).
+                        Add(PolynomialEasing.Quadratic, EasingType.Out, end, 1f);
+                    Rotation = rotation.Evaluate(AnimationCompletion);
+
+                    if (Time == (int)(UseTime * 0.7f))
+                        StartShakeAtPoint(Projectile.Center, 3f);
+
+                    Projectile.scale = InverseLerpBump(0f, 0.18f, 0.91f, 1f, AnimationCompletion);
+
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Calculates a quaternion from a given 2D and 3D side angle, taking into account this sword's <see cref="StartingRotation"/>.
+        /// </summary>
+        /// <param name="angle2D">The 2D angle.</param>
+        /// <param name="angleSide">The side angle.</param>
+        private Quaternion EulerAnglesConversion(float angle2D, float angleSide = 0f)
+        {
+            float forwardRotationOffset = angle2D * HorizontalDirection + (HorizontalDirection == -1f ? PiOver2 : 0f);
+            return Quaternion.CreateFromRotationMatrix(Matrix.CreateRotationZ(StartingRotation + forwardRotationOffset) * Matrix.CreateRotationX(angleSide));
         }
 
         public override bool ShouldUpdatePosition() => false;
 
         public override bool PreDraw(ref Color lightColor)
         {
-            // Draw the first fractal.
-            float rotation = Projectile.rotation + PiOver4;
             Texture2D texture = TextureAssets.Projectile[Type].Value;
-            Vector2 origin = Vector2.UnitY * texture.Size();
-            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
-            SpriteEffects direction = SpriteEffects.None;
+            Vector2 quadArea = texture.Size();
 
-            if (ForwardDirection == -1f)
-            {
-                origin.X = texture.Width - origin.X;
-                direction = SpriteEffects.FlipHorizontally;
-            }
+            // Calculate matrices that define the quad's orientation.
+            Matrix translation = Matrix.CreateTranslation(new Vector3(Projectile.Center.X - Main.screenPosition.X, Projectile.Center.Y - Main.screenPosition.Y, 0f));
+            Matrix projection = Matrix.CreateOrthographicOffCenter(0f, Main.screenWidth, Main.screenHeight, 0f, -100f, 100f);
+            Matrix view = translation * Main.GameViewMatrix.TransformationMatrix * projection;
+            Matrix rotation = Matrix.CreateFromQuaternion(Rotation);
+            Matrix scale = Matrix.CreateScale(Projectile.scale);
 
-            Main.EntitySpriteDraw(texture, drawPosition, null, Projectile.GetAlpha(Color.White), rotation, origin, Projectile.scale, direction);
+            // Generate the quads in a clockwise orientation.
+            VertexPositionColorTexture topLeft = new(new(0f, -quadArea.Y, 0f), Color.White, Vector2.One * 0.01f);
+            VertexPositionColorTexture topRight = new(new(quadArea.X, -quadArea.Y, 0f), Color.White, Vector2.UnitX * 0.99f);
+            VertexPositionColorTexture bottomLeft = new(new(0f, 0f, 0f), Color.White, Vector2.UnitY * 0.99f);
+            VertexPositionColorTexture bottomRight = new(new(quadArea.X, 0f, 0f), Color.White, Vector2.One * 0.99f);
+            VertexPositionColorTexture[] quad = [topLeft, topRight, bottomRight, bottomLeft];
+            short[] quadIndices = [0, 1, 2, 2, 3, 0];
+
+            // Draw the quads.
+            ManagedShader projectionShader = ShaderManager.GetShader("PrimitiveProjectionShader");
+            projectionShader.TrySetParameter("uWorldViewProjection", rotation * scale * view);
+            projectionShader.Apply();
+            Main.instance.GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            Main.instance.GraphicsDevice.Textures[1] = texture;
+            Main.instance.GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, quad, 0, quad.Length, quadIndices, 0, quadIndices.Length / 3);
 
             return false;
         }
